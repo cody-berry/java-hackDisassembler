020001168d1e059692e06d1f4f4b83bf73d4ec31
Cody <cody.tian@icloud.com>
2022-03-12 13:42:34 -0800

    translate A-instructions, binaryToDecimal()

--- src/Disassembler.java
+++ src/Disassembler.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.util.Arrays;
 
 import static processing.core.PApplet.loadStrings;
+import static processing.core.PApplet.parseInt;
 
 /**
  * @author cody
@@ -18,13 +19,35 @@ import static processing.core.PApplet.loadStrings;
 
 public class Disassembler {
 	public static void main(String[] args) {
-		String[] assemblyFile = loadStrings(new File("C:\\Users\\winry" +
+		String[] machineCodeFile = loadStrings(new File("C:\\Users\\winry" +
 				"\\Dropbox\\code\\java\\cody\\java-hackDisassembler\\src/Add.hack"));
 		int counter = 0;
-		for (String code : assemblyFile) {
-			System.out.println(String.format("%d: %s", counter, code));
+		for (String code : machineCodeFile) {
+			String translation;
+
+			System.out.print(String.format("%d: %s (commonly referred to as "
+					, counter, code));
 			counter++;
+			if (code.charAt(0) == '0') {
+				translation =
+						"@" + binaryToDecimal(code.substring(1, 16));
+			} else {
+				translation = "[Blank, it's a C]";
+			}
+			System.out.println(translation + ")");
 		}
 	}
 
+
+	// translates a 15-bit binary number into an integer
+	public static int binaryToDecimal(String binary) {
+		// our result is just a number. For each bit from right-to-left, we
+		// take 2 to the power of the index.
+		int result = 0;
+		for (int i = 0; i < binary.length(); i++) {
+			result += Math.pow(2,
+					i)*parseInt(String.valueOf(binary.charAt(14-i)));
+		}
+		return result;
+	}
 }
